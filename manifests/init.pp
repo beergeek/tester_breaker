1c89ec8e02a3c4a6e84660fe10c0dcca55fd5d96
beergeek <beergeek1679@gmail.com>
2019-10-09 04:08:46 +1100

    first commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,7 @@ class tester_breaker {
 
   file { '/tmp/xyz':
     ensure  => file,
-    content => Binary.new('aGVsbG8='),
+    content => Deferred(Binary('aGVsbG8=')),
     mode    => '0400',
   }
 }
