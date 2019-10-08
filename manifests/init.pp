f118b6598f1e338ece0f2d121948b9173c6422f0
beergeek <beergeek1679@gmail.com>
2019-10-08 11:14:32 +1100

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,15 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include tester_breaker
+class tester_breaker {
+
+    file { '/tmp/xyz':
+    ensure  => file,
+    content => Binary('O5jXGG0M7SmoXUJObZ/zSsqtis41JTDU'),
+    mode    => '0400',
+    require => Package['mongodb_mms_pkg'],
+  }
+}
